88bf17a70957cd562c961c4879787b094bd74005
cybersagittarius <cybersagittarius@gmail.com>
2021-06-22 16:31:59 +0200

    changed to Mail and SearchProvider

--- client/src/Components/Mail.jsx
+++ client/src/Components/Mail.jsx
@@ -1,16 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Form } from 'react-bootstrap';
+import axios from 'axios'
 import SearchContext from '../context/SearchContext';
 
-const MailForm = ({ toEmail }) => {
+
+const MailForm = ({ toEmail }) => {	
+
+	const [emailResponse, setEmailResponse] = useState('')
+
 	const context = useContext(SearchContext);
-	const { userInfo } = context;
+	const { userInfo, emailSubject, setEmailSubject, emailMessage, setEmailMessage } = context;
 	console.log(userInfo);
 	const { user } = userInfo;
 
-	const handleMail = (e) => {
+	const handleMail = async(e) => {
 		e.preventDefault();
-	};
+
+		const sendEmailMessage = {
+            fromEmail: user.email,
+            toEmail,
+            subject: emailSubject,
+			message: emailMessage,
+			username: user.username
+        };
+        //post to backend
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+           
+            const response = await axios.post(
+                'http://localhost:4000/search/send_email',
+                sendEmailMessage,
+                config
+            );
+
+			setEmailResponse(response.data.msg)
+            
+        } catch (err) {
+        console.log(`Something went wrong ${err}`);  
+				
+        }
+    };
 
 	return (
 		<>
@@ -23,16 +56,19 @@ const MailForm = ({ toEmail }) => {
 						<p className="mailBorder">{toEmail}</p>
 					</div>
 					<Form.Label> Subject </Form.Label>{' '}
-					<Form.Control type="email" placeholder="title of your message" />
+					<Form.Control onChange = {(e) => setEmailSubject(e.target.value) } type="text" name="subject" placeholder="title of your message" />
 					<Form.Label> Description </Form.Label>{' '}
 					<Form.Control
 						className="descriptionMail"
-						type="email"
+						type="text"
 						placeholder="type your message"
+						onChange = {(e) => setEmailMessage(e.target.value) } 
 					/>
 				</Form.Group>{' '}
 				<button type="submit" className="sendMail">send</button>
 			</Form>{' '}
+
+			{ emailResponse.length > 0 && <p>{ emailResponse }</p> }
 					</>
 	);
 };
