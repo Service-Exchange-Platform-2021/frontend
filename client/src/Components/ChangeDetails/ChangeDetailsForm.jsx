ba85700984274404dd2ab1c54b218132bd02735f
NargizaRA <rysmendieva.nargiza@gmail.com>
2021-06-28 10:50:47 +0200

    Media query for Profile

--- client/src/Components/ChangeDetails/ChangeDetailsForm.jsx
+++ client/src/Components/ChangeDetails/ChangeDetailsForm.jsx
@@ -1,37 +1,40 @@
 import React, { useRef, useEffect, useContext } from "react"
-import SkillsMenuLarge from '../SkillsMenuLarge'
+import SkillsMenu from '../SkillsMenu'
 import items from '../../assets/data/itemsSkills'
 import MyAlert from "../userControl/layout/Alert";
-import SearchContext from "../../context/SearchContext";
-
 import { Link } from "react-router-dom";
 
-//3rd party package
 import RegionCountrySelector from "../userControl/selector/RegionCountrySelector";
 import AvatarUploader from "../userControl/upload-edit/AvatarUploader";
 
 
 const ChangeDetailsForm = (props) => {
 
+
+
   const firstnameRef = useRef()
+
 
   useEffect(() => {
     firstnameRef.current.focus();
 
   }, []);
 
+
+
+
   return (
     <>
       <div className="backRegister">
         <div className="card div-center-details ">
           <div className="container">
-            <div className="card-body">
+            <div className="details-body">
 
 
               <br />
               <form onSubmit={props.submitHandler}>
                 <div className="form-row">
-                  <div className="form-group col-lg-6">
+                  <div className="form-group col-lg-11 details">
                     <label>First Name</label>
 
                     <input
@@ -46,7 +49,7 @@ const ChangeDetailsForm = (props) => {
                     />
                   </div>
 
-                  <div className="form-group col-lg-6">
+                  <div className="form-group col-lg-11 details">
                     <label>Last Name</label>
 
                     <input
@@ -60,20 +63,7 @@ const ChangeDetailsForm = (props) => {
                     />
                   </div>
 
-                  <div className="form-group col-lg-10">
-                    <label>Country</label>
-
-                    <RegionCountrySelector
-                      country={props.country}
-                      region={props.region}
-                      setParentCountry={(val) => props.changeCountry(val)}
-                      setParentRegion={(val) => props.changeRegion(val)}
-                    />
-
-                  </div>
-
-
-                  <div className="form-group col-lg-5">
+                  <div className="form-group col-lg-11 details">
                     <label>User Name</label>
 
                     <input
@@ -87,52 +77,64 @@ const ChangeDetailsForm = (props) => {
                     />
                   </div>
 
-                  <div className="form-group col-lg-4">
-                    <label>
-                      Skills I can offer
-                    </label>
+                  <div className="form-group col-lg-11 details">
+                    <label>Country & Region</label>
 
-
-                    <SkillsMenuLarge
-                      // title="I'm looking for" 
-                      className="form-control"
-                      items={items}
-                      multiSelect
-                      selection={props.offerSelection}
-                      handleSelection={props.changeOfferSelection} />
-                    {props.offerSelection.length > 0 ?
-                      props.offerSelection.map(item => {
-                        return <div key={item.id}>
-                          <h4>{item.value}</h4>
-                        </div>
-                      })
-                      : null
-                    }
-
-                  </div>
-
-                  <div className="form-group col-lg-4">
-                    <label>
-                      Avatar: Maximal Upload File Size 80 KB
-                    </label>
-
-                    <AvatarUploader
-                      // input name="avatar" type='file' accept='image/*'
-                      // onClose={props.onClose}
-                      onCrop={props.onCrop}
-                      onBeforeFileLoad={props.onBeforeFileLoad}
-                      preview={props.preview}
-                      savedImage={props.savedImage}
+                    <RegionCountrySelector
+                      country={props.country}
+                      region={props.region}
+                      setParentCountry={(val) => props.changeCountry(val)}
+                      setParentRegion={(val) => props.changeRegion(val)}
                     />
 
                   </div>
 
-                  <div className="form-group col-lg-12 d-flex justify-content-end">
+
+
+
+                  <div className="form-group col-lg-11 details">
+                    <label>
+                      Skills I can offer</label>
+
+                    <SkillsMenu
+                      items={items}
+                      multiSelect
+                      selection={props.offerSelection}
+                      handleSelection={props.changeOfferSelection} />
+                    <div className="skills outcome">
+                      {props.offerSelection.length > 0 ?
+                        props.offerSelection.map(item => {
+                          return <div key={item.id}>
+                            <h4>{item.value}</h4>
+                          </div>
+                        })
+                        : null
+                      }
+                    </div>
+                  </div>
+
+                  <div className="form-group col-lg-11 details">
+                    <label>
+                      Avatar: Maximal Upload File Size 80 KB
+                    </label>
+                    <div className="uploader">
+                      <AvatarUploader
+                        // input name="avatar" type='file' accept='image/*'
+                        // onClose={props.onClose}
+                        onCrop={props.onCrop}
+                        onBeforeFileLoad={props.onBeforeFileLoad}
+                        preview={props.preview}
+                        savedImage={props.savedImage}
+                      />
+
+                    </div></div>
+
+                  <div className="form-group col-lg-12 d-flex justify-content-center">
                     <button type="submit" className="btn btn-primary btn-sm">
                       Submit
                     </button>
                   </div>
-                  <div className="form-group col-lg-12 d-flex justify-content-end">
+                  <div className="form-group col-lg-12 d-flex justify-content-center">
                     <button className="btn btn-primary btn-md"><Link to="/profile">Back to profile</Link>
                     </button>
                   </div>
